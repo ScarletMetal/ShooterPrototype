342c6d64fb589b5735d9e69964f758df107a07f6
Spikes-2212 <spikes.no2212@gmail.com>
2018-11-03 10:55:40 +0200

    added SubsystemComponents

---
+++ src/com/spikes2212/robot/SubsystemComponents.java
@@ -0,0 +1,14 @@
+package com.spikes2212.robot;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class SubsystemComponents {
+	public static class Shooter { 
+		public static WPI_TalonSRX ShooterTalon1 = new WPI_TalonSRX(RobotMap.CAN.SHOOTER_1);
+		public static WPI_TalonSRX ShooterTalon2 = new WPI_TalonSRX(RobotMap.CAN.SHOOTER_2);
+		
+		static {
+			
+		}
+	}
+}
