eab4ff1760b9e93e5dcb235fddecfa097ab8995b
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-11-03 12:49:10 +0200

    added encoder initialization

--- src/com/spikes2212/robot/SubsystemComponents.java
+++ src/com/spikes2212/robot/SubsystemComponents.java
@@ -1,14 +1,20 @@
 package com.spikes2212.robot;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.spikes2212.robot.components.TalonSRXEncoder;
 
 public class SubsystemComponents {
 	public static class Shooter { 
-		public static WPI_TalonSRX ShooterTalon1 = new WPI_TalonSRX(RobotMap.CAN.SHOOTER_1);
-		public static WPI_TalonSRX ShooterTalon2 = new WPI_TalonSRX(RobotMap.CAN.SHOOTER_2);
-		
+		public static final WPI_TalonSRX ShooterTalon1 = new WPI_TalonSRX(RobotMap.CAN.SHOOTER_1);
+		public static final WPI_TalonSRX ShooterTalon2 = new WPI_TalonSRX(RobotMap.CAN.SHOOTER_2);
+		public static TalonSRXEncoder encoder;
+		public static final int encoderKTimeoutms = 30;
 		static {
-//			ShooterTalon1.setInverted(true);
+			ShooterTalon1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, encoderKTimeoutms);
+			ShooterTalon1.setSensorPhase(true);
+
+			encoder = new TalonSRXEncoder(() -> (double) ShooterTalon1.getSelectedSensorVelocity());
 		}
 	}
 }
