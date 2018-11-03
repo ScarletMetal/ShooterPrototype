5a912f25abfb4108f9d47a513e9c5845635506dc
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2018-11-03 12:45:56 +0200

    added TalonSRXEncoder component

---
+++ src/com/spikes2212/robot/components/TalonSRXEncoder.java
@@ -0,0 +1,30 @@
+package com.spikes2212.robot.components;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+import java.util.function.Supplier;
+
+public class TalonSRXEncoder implements PIDSource {
+
+    private Supplier<Double> velocitySupplier;
+    public TalonSRXEncoder(Supplier<Double> velocitySupplier) {
+        this.velocitySupplier = velocitySupplier;
+    }
+
+    @Override
+    public void setPIDSourceType(PIDSourceType pidSource) {
+
+    }
+
+    @Override
+    public PIDSourceType getPIDSourceType() {
+        return null;
+    }
+
+    @Override
+    public double pidGet() {
+        return velocitySupplier.get();
+    }
+}
